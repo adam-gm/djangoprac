30c2e437073c2b6f6a05415906959284174b5ff6
adam-gm <adam01.moumen@gmail.com>
2025-06-20 03:26:43 +0200

    Make settings.py visible and added env. Dynamic urls in html template by adding  namespace to urlconf. Implemented shortcuts for views.py return

--- djangotutorial/polls/urls.py
+++ djangotutorial/polls/urls.py
@@ -3,11 +3,16 @@ from . import views
 
 # When reaching this url through the web browser, the HttpResponse from views.index will be sent to the web page.
 # "" in path indicates that the url stops at 127.0.0.1:8000/polls/
+
+
+# Namespace for urlconfig so Django knows which views function to choose, if different app path is given same name.
+app_name = "polls"
+
 urlpatterns = [
     # ex: /polls/
     path("", views.index, name="index"),
     # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
+    path("specifics/<int:question_id>/", views.detail, name="detail"),
     # ex: /polls/5/results/
     path("<int:question_id>/results/", views.results, name="results"),
     # ex: /polls/5/vote/
